306f4a6e791dbcd80e2ee9e477608fe268d2da54
kh <uot@windowslive.com>
2019-07-23 08:59:20 +0300

    First Comment

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+'''
+Each model is a Python class that subclasses django.db.models.Model.
+Each attribute of the model represents a database field.
+With all of this, Django gives you an automatically-generated database-access API; see Making queries.
+
+'''
+from django.db import models
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    opening_time = models.CharField(max_length=30) #DateTimeField
+    closing_time = models.CharField(max_length=30) # DateTimeField
+    created_on = models.DateTimeField(auto_now_add=True)
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Restaurant
 def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
@@ -24,10 +24,38 @@ def restaurant_list(request):
 
 
 def restaurant_detail(request):
+
+    res = Restaurant()
+    res.name = "Al Baik"
+    res.description = "Fast Food"
+    res.opening_time = "11:00 AM"
+    res.closing_time = "12:00 PM"
+    res.save()
+
+    '''
+    rest2 = Restaurant.objects.create(name="AlSorror")
+    rest2.name = "AlSorror"
+    rest2.description = "mndy"
+    rest2.opening_time="01:00 PM"
+    rest2.closing_time="12:00 PM"
+    rest2.save()
+    Restaurant.objects.values_list('name', flat=True)
+    '''
+
+    res = 'Printing all Dreamreal entries in the DB : <br>'
+    b = Restaurant.objects.all()
+    attachment = {"my_object": {}}
+    attachment['my_object'] = b[1]
+    #attachment['my_object'] = b.description
+    #attachment['my_object'] = b.opening_time
+    #attachment['my_object'] = b.closing_time
+
+
+
     context = {
         "my_object":{
                         'name':'Subway',
                         'food_type':'Sandwiches.',
                     },
     }
-    return render(request, 'detail.html', context)
+    return render(request, 'detail.html', attachment)
